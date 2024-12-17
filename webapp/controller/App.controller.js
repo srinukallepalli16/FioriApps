6891f16cb6a7b35152d8b4a515fbdf7f587aa93b
Srinu Kallepalli <srinukallepalli16@gmail.com>
2024-12-17 16:01:32 +0000

    first commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.app.assignment.controller.App", {
+        onInit() {
+        }
+    });
+});
