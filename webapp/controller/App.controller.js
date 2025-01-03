31fec15ef5e988c0db9cebd570c547947892708b
Srinu Kallepalli <srinukallepalli16@gmail.com>
2025-01-03 13:55:21 +0000

    first commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("logins.controller.App", {
+        onInit() {
+            var obj ={
+                "data":{
+                   name :"srinu"
+                }
+            }
+           var oModel = new sap.ui.model.json.JSONModel(obj);
+           this.getView().setModel(oModel,"oModel");
+        },
+        loginpress:function(){
+           var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+           oRouter.navTo("Routedashboard");
+        }
+        // navbutton:function(){
+        //     var oNavto = sap.ui.core.UIComponent.getRouterFor(this);
+        //     oNavto.navTo("RouteApp");
+        // }
+    });
+});
