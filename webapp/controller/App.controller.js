f1c19abb6809ee44d606f6aa961b345bbc477ba9
Srinu Kallepalli <srinukallepalli16@gmail.com>
2025-03-20 11:32:01 +0000

    jsonmodel

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,91 +5,17 @@ sap.ui.define([
 
     return Controller.extend("com.sap.mybankdetails.controller.App", {
         onInit() {
-            debugger
-
-            let oData = {
-                "AccountDetails": {
-                    AccountNum: "12345678",
-                    Name: "Srinu Kallepalli",
-                    IFSCCode: "0987654",
-                    CustomerId:"123******",
-                    Address :{
-                        city:"Srikakulam",
-                        postalcode:"12345",
-                        country:"USA"
-                    }
-                },
-                // "ifsccode": "098765454"
-               "cardDetails":[
-                   {
-                    CardCompany:"Master Card",
-                    cardtype:"Credit Card",
-                    cardnumber:"1111111111",
-                    assignmentdate:"assigned on 12 oct 2024",
-                    state:false
-
-                   },
-                   {
-                    CardCompany:"Master Card",
-                    cardtype:"Debit Card",
-                    cardnumber:"453627891",
-                    assignmentdate:"assigned on 12 nov 2022",
-                    state:true
-
-                   },
-                   {
-                    CardCompany:"Master Card",
-                    cardtype:"Debit Card",
-                    cardnumber:"8759363738",
-                    assignmentdate:"assigned on 16 march 2023",
-                    state:false
-
-                   },
-                   {
-                    CardCompany:"Master Card",
-                    cardtype:"Credit Card",
-                    cardnumber:"62372376327",
-                    assignmentdate:"assigned on 19 jun 2025",
-                    state:true
-
-                   }
-               ]
-
-            };
-            
-      
-            let oModel = new sap.ui.model.json.JSONModel();
-            oModel.setData(oData);
-            this.getView().setModel(oModel,"oBankDetails");
             // debugger
-            /*
-            if (navigator.language == "en-US") {
-                var i18nModel = this.getOwnerComponent().getModel("i18n_es");
-                this.getOwnerComponent().setModel(i18nModel, "i18n");
-            } else {
-                var i18nModel = this.getOwnerComponent().getModel("i18n");
-                this.getOwnerComponent().setModel(i18nModel, "i18n");
-            }
-            */
-            /* Checking browser language and setting 
-            the global resource model*/
-            var appLang;
-            if (navigator.language == 'en-US1') {
-                appLang = "i18n_es";
-                var i18nModel = this.getOwnerComponent().getModel(appLang);
-                this.getOwnerComponent().setModel(i18nModel, "i18n");
-            }
-            else if (navigator.language == 'en-US') {
-                appLang = "i18n";
-                var i18nModel = this.getOwnerComponent().getModel(appLang);
-                this.getOwnerComponent().setModel(i18nModel, "i18n");
-            }
-            else {
-                appLang = "i18n"
-                var i18nModel = this.getOwnerComponent().getModel(appLang);
-                this.getOwnerComponent().setModel(i18nModel, "i18n");
-            }
-
+        /*Setting Global JSON Model*/
+         this._setGlobalModel();
+        /* Checking browser language and setting the global resource model*/
+         this._setGlobalLanguage();
+         /*
+         let oProfileModel = new sap.ui.model.json.JSONModel(
+            {profile: sap.ui.require.toUrl(com/sap/mybankdetails/images/profile1.jpg)}
+        );
+        this.getView().setModel(oProfileModel);
+        */
         },
         openbankdetails: function () {
             //create dailog lazily
@@ -106,7 +32,49 @@ sap.ui.define([
         },
         onCloseBankDetail: function () {
             this.byId("morebankDetails").close();
+        },
+        _setGlobalModel:function(){
+            let oModel = this.getOwnerComponent().getModel("oBankDetails");
+            this.getView().setModel(oModel);
+        },
+        _setGlobalLanguage:function(){
+            var appLang;
+            if (navigator.language == 'en-US1') {
+                appLang = "i18n_es";
+                var i18nModel = this.getOwnerComponent().getModel(appLang);
+                this.getOwnerComponent().setModel(i18nModel, "i18n");
+            }
+            else if (navigator.language == 'en-US') {
+                appLang = "i18n";
+                var i18nModel = this.getOwnerComponent().getModel(appLang);
+                this.getOwnerComponent().setModel(i18nModel, "i18n");
+            }
+            else {
+                appLang = "i18n"
+                var i18nModel = this.getOwnerComponent().getModel(appLang);
+                this.getOwnerComponent().setModel(i18nModel, "i18n");
+            }
         }
 
     });
 });
+
+  /*
+let oData = {
+"ifsccode": "098765454"
+}
+            
+      
+let oModel = new sap.ui.model.json.JSONModel();
+oModel.setData(oData);
+this.getView().setModel(oModel,"oBankDetails");
+// debugger
+ /*
+  if (navigator.language == "en-US") {
+var i18nModel = this.getOwnerComponent().getModel("i18n_es");
+this.getOwnerComponent().setModel(i18nModel, "i18n");
+} else {
+var i18nModel = this.getOwnerComponent().getModel("i18n");
+this.getOwnerComponent().setModel(i18nModel, "i18n");
+}
+*/
